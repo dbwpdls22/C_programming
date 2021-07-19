c8a41d60d8414b9bf0a72003ecddd9cc5c04984e
dbwpdls22 <43002719+dbwpdls22@users.noreply.github.com>
2021-07-19 21:22:49 +0900

    Add files via upload

---
+++ 02/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char c;
+	
+	printf("input a character :");
+	scanf("%c", &c);
+	
+	printf("The next character of %c (%i) is %c (%i))\n", c, c, c+1, c+1);
+
+	return 0;
+}
