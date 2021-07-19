2bfa5978c00e90d5861b3cd777a2282f94b3db5e
dbwpdls22 <43002719+dbwpdls22@users.noreply.github.com>
2021-07-19 21:24:09 +0900

    Add files via upload
    operator

---
+++ 03/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int in1, in2, in3;
+	
+	printf("input 3 integers: ");
+    scanf("%d %d %d", &in1, &in2, &in3);
+
+	printf("average of %i %i %i is %f\n", in1, in2, in3,
+	                                (double)(in1+in2+in3)/3);
+ 	
+	 
+	return 0;
+}
