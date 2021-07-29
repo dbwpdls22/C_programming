e5ac84ab0a50a0d22f2bf3bb01819f1e481cb54d
dbwpdls22 <43002719+dbwpdls22@users.noreply.github.com>
2021-07-29 22:01:52 +0900

    Add files via upload

---
+++ 07/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void print_image(int image[5][5])
+{
+	int i,j;
+	int *ptr= &image[0][0];
+	
+	for (i=0; i<5; i++)
+	{
+		for(j=0;j<5;j++)
+		{
+			printf("%d",*ptr);
+			ptr=ptr+1;
+		}
+		printf("\n");
+		
+	}
+}
+
+void brighten_image(int image[5][5])
+{
+	int i,j;
+	int *ptr= &image[0][0];
+	
+	for (i=0; i<5; i++)
+	{
+		for(j=0;j<5;j++)
+		{
+			*ptr= *ptr+10;
+			ptr=ptr+1;
+			
+		}
+		
+		
+	}
+}
+int main(int argc, char *argv[]) {
+	
+	int image [5][5] = {
+	{10,20,30,40,50},
+	{10,20,30,40,50},
+	{10,20,30,40,50},
+	{10,20,30,40,50},
+	{10,20,30,40,50}
+	};
+	
+	print_image(image);
+	brighten_image(image);
+	print_image(image);
+	
+	return 0;
+}
